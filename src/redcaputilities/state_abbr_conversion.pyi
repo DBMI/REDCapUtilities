30ff850e45fe6f0ce6c8895576824e31551080e8
kevin-dbmi <kjdelaney@ucsd.edu>
2026-08-21 10:41:20 -0700

    Move StateAbbreviationConverter to this project

---
+++ src/redcaputilities/state_abbr_conversion.pyi
@@ -0,0 +1,8 @@
+from typing import Any
+
+class StateAbbreviationConverter:
+    def __init__(self) -> None:
+        self.__states = None
+        ...
+
+    def full_name(self, two_letter_code: Any): ...
